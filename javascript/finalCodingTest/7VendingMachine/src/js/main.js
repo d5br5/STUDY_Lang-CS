f1093854e693ff527d429a19fae61d211ca8a544
DohyungKim <ehgud456456@naver.com>
2021-12-29 00:46:58 +0900

    final coding test ch7

---
+++ javascript/finalCodingTest/7VendingMachine/src/js/main.js
@@ -0,0 +1,8 @@
+import ColaGenerator from "./components/colaGenerator.js";
+import VendingMachine from "./components/vendingMachine.js";
+
+const CG = new ColaGenerator();
+const VM = new VendingMachine();
+
+CG.setup();
+VM.setup();
