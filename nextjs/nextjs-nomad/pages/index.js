1c286e8f1ea11a66a534e4e372b84118c5d8dab3
DohyungKim <ehgud456456@naver.com>
2022-01-07 01:35:19 +0900

    next js init

---
+++ nextjs/nextjs-nomad/pages/index.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<div>
+			<NavBar />
+			<h1>hello {counter}</h1>
+			<button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+		</div>
+	);
+}
