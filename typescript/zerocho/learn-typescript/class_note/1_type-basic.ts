06bea1d00b9a4b1b27cbbd78e7c0949e828a0289
DohyungKim <ehgud456456@naver.com>
2022-01-29 17:01:55 +0900

    ts type

---
+++ typescript/zerocho/learn-typescript/class_note/1_type-basic.ts
@@ -0,0 +1,19 @@
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["cpart", "Thos", "10"];
+let items: number[] = [1, 2, 3];
+
+let address: [string, string, number] = ["gangnam", "pangyo", 10];
+let add1: [string[], [number, string]] = [
+	["string", "string"],
+	[10, "string"],
+];
+
+let obj: object = {};
+
+type person = {name: string; age: number};
+let person: person = {
+	name: "capt",
+	age: 100,
+};
+
+let show: boolean = true;
---
+++ typescript/zerocho/learn-typescript/class_note/2_functions.ts
@@ -0,0 +1,6 @@
+function sum(a, b?: number) {
+	return a + b;
+}
+
+sum(10, 20);
+sum(10);
