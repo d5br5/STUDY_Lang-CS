85a1a8b9e41a7f503a7d5233f794a2ed476e00d6
Doh Kim <ehgud456456@naver.com>
2022-02-10 15:14:32 +0900

    init vuestagram

---
+++ vuejs/vuestagram/src/components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<Post />
+		<Post />
+		<Post />
+	</div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+	name: "postbox",
+	components: { Post },
+};
+</script>
+
+<style></style>
