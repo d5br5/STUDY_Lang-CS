077e99bd96952960ebf151fde5e4d90d13e456ed
DohyungKim <ehgud456456@naver.com>
2022-03-23 21:51:28 +0900

    [lec] coding apple free

---
+++ language/javascript/typescript/codingapple/index.js
@@ -0,0 +1,32 @@
+var names = "kim";
+var age = 50;
+var marry = true;
+var undefin = undefined;
+var nul = null;
+var member = ["kim", "park", 1];
+var members = {
+    name: "kim",
+    address: "seoul",
+    age: 1,
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
+var ants = "string";
+var var1 = ants;
+var user = "kim";
+var age1 = undefined;
+var married = false;
+var chulsoo = [user, age1, married];
+var school = {
+    score: [100, 97, 876],
+    teacher: "phil",
+    friend: "john",
+};
+school.score[4] = false;
+school.friend = ["Lee", school.teacher];
+function myfunction(x) { }
+myfunction(2);
+myfunction("string");
---
+++ language/javascript/typescript/codingapple/index.ts
@@ -0,0 +1,56 @@
+let names: string = "kim";
+let age: number = 50;
+let marry: boolean = true;
+let undefin: undefined = undefined;
+let nul = null;
+
+type MemberType = { name: string; address: string; age: number };
+let member: (string | number)[] = ["kim", "park", 1];
+let members: MemberType = {
+	name: "kim",
+	address: "seoul",
+	age: 1,
+};
+
+type ProjectType = {
+	member: string[];
+	days: number;
+	started: boolean;
+};
+let project: ProjectType = {
+	member: ["kim", "park"],
+	days: 30,
+	started: true,
+};
+
+let ants: unknown = "string";
+let var1: unknown = ants;
+
+let user: string = "kim";
+let age1: number | undefined = undefined;
+let married: boolean = false;
+let chulsoo: [string, number | undefined, boolean] = [user, age1, married];
+
+let school: {
+	score: (number | boolean)[];
+	teacher: string;
+	friend: string | string[];
+} = {
+	score: [100, 97, 876],
+	teacher: "phil",
+	friend: "john",
+};
+school.score[4] = false;
+school.friend = ["Lee", school.teacher];
+
+function myfunction(x?: number | string): void {}
+myfunction(2);
+myfunction("string");
+
+function hello(name?: string): void {
+	if (name === undefined) {
+		console.log("hi");
+	} else {
+		console.log("noname");
+	}
+}
