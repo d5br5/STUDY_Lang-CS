c5c6447de37257144bf2373041d26fb4a626f0aa
Doh Kim <ehgud456456@naver.com>
2022-04-04 18:14:57 +0900

    [lec] 구구단

---
+++ language/javascript/typescript/zerocho/lecture/client.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import GuGuDan from "./lec/GuGuDan";
+import WordRelay from "./lec/WordRelay";
+
+ReactDOM.render(<WordRelay />, document.querySelector("#root"));
