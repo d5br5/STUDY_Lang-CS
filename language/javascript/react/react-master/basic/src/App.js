818eaec2ed3756ee6e00eac422d180e27f1af3f7
DohyungKim <ehgud456456@naver.com>
2022-05-20 22:37:29 +0900

    RCT ch05

--- language/javascript/react/react-master/basic/src/App.js
+++ language/javascript/react/react-master/basic/src/App.js
@@ -1,7 +1,17 @@
-import EventPractice from "./EventPractice";
+import ScrollBox from "./05Ref/ScrollBox";
+import { Component } from "react";
 
-function App() {
-	return <EventPractice />;
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<ScrollBox ref={(ref) => (this.scrollbox = ref)} />
+				<button onClick={() => this.scrollbox.scrollToBottom()}>
+					맨 밑으로
+				</button>
+			</div>
+		);
+	}
 }
 
 export default App;
