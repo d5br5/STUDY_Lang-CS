528501ec024ca72d9f3ea9b77964583766982f1d
DohyungKim <ehgud456456@naver.com>
2022-06-12 21:04:24 +0900

    RCT ch 18 - Saga

--- language/javascript/react/react-master/redux-middleware/src/modules/index.js
+++ language/javascript/react/react-master/redux-middleware/src/modules/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from "redux";
 import counter, { counterSaga } from "./counter";
-import sample from "./sample";
+import sample, { sampleSaga } from "./sample";
 import loading from "./loading";
 import { all } from "redux-saga/effects";
 
 const rootReducer = combineReducers({ counter, sample, loading });
 
 export function* rootSaga() {
-	yield all([counterSaga()]);
+	yield all([counterSaga(), sampleSaga()]);
 }
 
 export default rootReducer;
