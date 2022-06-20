bed45b2a5b3a5c9a067274818e91084053bce683
Doh Kim <ehgud456456@naver.com>
2022-06-20 16:11:58 +0900

    RCT ch 23

--- language/javascript/react/react-master/blog/blog-backend/src/api/posts/index.js
+++ language/javascript/react/react-master/blog/blog-backend/src/api/posts/index.js
@@ -1,20 +1,21 @@
 import Router from "koa-router";
 import * as postsCtrl from "./posts.ctrl";
+import checkLoggedIn from "../../lib/checkLoggedIn";
 
 const posts = new Router();
 
 posts.get("/", postsCtrl.list);
-posts.post("/", postsCtrl.write);
+posts.post("/", checkLoggedIn, postsCtrl.write);
 
-posts.get("/:id", postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete("/:id", postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch("/:id", postsCtrl.checkObjectId, postsCtrl.update);
+// posts.get("/:id", postsCtrl.checkObjectId, postsCtrl.read);
+// posts.delete("/:id", postsCtrl.checkObjectId, postsCtrl.remove);
+// posts.patch("/:id", postsCtrl.checkObjectId, postsCtrl.update);
 
 const post = new Router();
 post.get("/", postsCtrl.read);
-post.delete("/", postsCtrl.remove);
-post.patch("/", postsCtrl.update);
+post.delete("/", checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch("/", checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
-posts.use("/:id", postsCtrl.checkObjectId, post.routes());
+posts.use("/:id", postsCtrl.getPostById, post.routes());
 
 export default posts;
