782022878e80880004a61dc3194ab7dc171032ae
DohyungKim <ehgud456456@naver.com>
2023-01-08 22:27:22 +0900

    go, pipe, curry

--- language/javascript/functional/map_filter_reduce.js
+++ language/javascript/functional/map_filter_reduce.js
@@ -9,23 +9,27 @@ const products = [
 	{ name: "신발", price: 35000 },
 ];
 
-const map = (f, iter) => {
+const curry =
+	(f) =>
+	(a, ..._) =>
+		_.length ? f(a, ..._) : (..._) => f(a, ..._);
+const map = curry((f, iter) => {
 	let res = [];
 	for (const a of iter) {
 		res.push(f(a));
 	}
 	return res;
-};
+});
 
-const filter = (f, iter) => {
+const filter = curry((f, iter) => {
 	let res = [];
 	for (const a of iter) {
 		if (f(a)) res.push(a);
 	}
 	return res;
-};
+});
 
-const reduce = (f, acc, iter) => {
+const reduce = curry((f, acc, iter) => {
 	if (!iter) {
 		iter = acc[Symbol.iterator]();
 		acc = iter.next().value;
@@ -34,7 +38,7 @@ const reduce = (f, acc, iter) => {
 		acc = f(acc, a);
 	}
 	return acc;
-};
+});
 
 let result = reduce(
 	(a, b) => a + b,
@@ -45,3 +49,42 @@ let result = reduce(
 );
 
 log(result);
+
+const go = (...args) => reduce((a, f) => f(a), args);
+
+const pipe =
+	(f, ...fs) =>
+	(...as) =>
+		go(f(...as), ...fs);
+
+go(
+	0,
+	(a) => a + 1,
+	(a) => a + 10,
+	(a) => a + 100,
+	log
+);
+
+const f = pipe(
+	(a = 0, b = 0) => a + b,
+	(a) => a + 10,
+	(a) => a + 100
+);
+
+log(f());
+const add = (a, b) => a + b;
+const total_price = pipe(
+	map((p) => p.price),
+	reduce(add)
+);
+
+const base_total_price = (predi) => pipe(filter(predi), total_price);
+
+go(
+	products,
+	base_total_price((p) => p.price < 20000),
+	log
+);
+
+const multi = curry((a, b) => a * b);
+log(multi(4)(5));
