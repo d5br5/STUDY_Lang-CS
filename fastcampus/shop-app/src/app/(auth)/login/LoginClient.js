bda6097aea363574a483f69b3abf2eb84b90af08
DohyungKim <ehgud456456@naver.com>
2023-09-29 12:03:38 +0900

    [Feat] login, loader UI

---
+++ fastcampus/shop-app/src/app/(auth)/login/LoginClient.js
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import LogoPath from "@/assets/colorful.svg";
+
+import styles from "./Auth.module.scss";
+import Loader from "@/components/loader/Loader";
+
+const LoginClient = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
+
+  const router = useRouter();
+  const redirectUser = () => {
+    router.push("/");
+  };
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
+
+  const signInWithGoogle = () => {};
+
+  return (
+    <>
+      {<Loader />}
+
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image src={LogoPath} alt="logo" width={247} />
+          </h1>
+          <form className={styles.form} onSubmit={loginUser}>
+            Input
+            <div className={styles.group}>자동 로그인, 비밀번호 수정</div>
+            <div className={styles.buttonGroup}>
+              Button
+              <div>Button</div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LoginClient;
