d1005663bc06cacd1cef1dc2d9eaef2b7d24bab7
DohyungKim <ehgud456456@naver.com>
2023-09-29 21:26:12 +0900

    [Feat] button, divider

--- fastcampus/shop-app/src/app/(auth)/login/LoginClient.js
+++ fastcampus/shop-app/src/app/(auth)/login/LoginClient.js
@@ -8,6 +8,9 @@ import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
 import Input from "@/components/input/Input";
 import AutoSignInCheckbox from "@/components/autoSignInCheckbox/AutoSignInCheckbox";
+import Divider from "@/components/divider/Divider";
+import Button from "@/components/button/Button";
+import Link from "next/link";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -67,8 +70,17 @@ const LoginClient = () => {
               />
             </div>
             <div className={styles.buttonGroup}>
-              Button
-              <div>Button</div>
+              <Button type="submit" width="100%">
+                로그인
+              </Button>
+              <Divider />
+              <Button width="100%" secondary>
+                <Link href={"/register"}>회원가입</Link>
+              </Button>
+              <Divider />
+              <div>
+                <Button onClick={signInWithGoogle}>구글 로그인</Button>
+              </div>
             </div>
           </form>
         </div>
--- fastcampus/shop-app/src/components/divider/Divider.js
+++ fastcampus/shop-app/src/components/divider/Divider.js
@@ -1,7 +1,26 @@
+import classNames from "classnames";
 import React from "react";
+import styles from "./Divider.module.scss";
 
-const Divider = () => {
-  return <div>Divider</div>;
+const Divider = ({
+  space = 22,
+  color = "#CCC",
+  className = "",
+  ...restProps
+}) => {
+  const style = {
+    marginTop: space,
+    marginBottom: space,
+    background: color,
+  };
+  return (
+    <div
+      role="presentation"
+      className={classNames(styles.line, className)}
+      style={style}
+      {...restProps}
+    />
+  );
 };
 
 export default Divider;
