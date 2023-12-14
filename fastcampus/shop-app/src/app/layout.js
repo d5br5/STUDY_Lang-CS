6eae97a1f9ed83725e96d0569758ade6c077c80e
DohyungKim <ehgud456456@naver.com>
2023-12-15 02:34:20 +0900

    [Feat] Header, Footer

--- fastcampus/shop-app/src/app/layout.js
+++ fastcampus/shop-app/src/app/layout.js
@@ -1,6 +1,8 @@
 import ToastProvider from "@/components/toastProvider/ToastProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Footer from "@/layouts/footer/Footer";
+import Header from "@/layouts/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ToastProvider />
+        <Header />
         {children}
+        <Footer />
         <div id="portal"></div>
       </body>
     </html>
--- fastcampus/shop-app/src/layouts/header/Header.js
+++ fastcampus/shop-app/src/layouts/header/Header.js
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import styles from "./Header.module.scss";
+import Link from "next/link";
+
+import InnerHeader from "../innerHeader/InnerHeader";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutUser = () => {};
+
+  return (
+    <header>
+      <div className={styles.loginBar}>
+        <ul className={styles.list}>
+          {!isLoggedIn ? (
+            <li className={styles.item}>
+              <Link href={"/login"}>로그인</Link>
+            </li>
+          ) : (
+            <>
+              <li className={styles.item}>
+                <Link href={"/admin/dashboard"}>관리자</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/order-history"}>주문 목록</Link>
+              </li>
+              <li className={styles.item}>
+                <Link href={"/"} onClick={logoutUser}>
+                  로그아웃
+                </Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>제휴 마케팅</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>쿠팡 플레이</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>고객센터</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      {/* {pathname.startsWith("/admin") ? null : <InnerHeader />} */}
+    </header>
+  );
 };
 
 export default Header;
