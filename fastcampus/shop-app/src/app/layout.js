0784763559e631acba62bd82f8033359544d509b
DohyungKim <ehgud456456@naver.com>
2023-12-14 15:38:52 +0900

    [Feat] react-toastify

--- fastcampus/shop-app/src/app/layout.js
+++ fastcampus/shop-app/src/app/layout.js
@@ -1,3 +1,4 @@
+import ToastProvider from "@/components/toastProvider/ToastProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ToastProvider />
         {children}
         <div id="portal"></div>
       </body>
